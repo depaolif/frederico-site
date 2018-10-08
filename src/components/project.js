2621b2cc65c655316f90e0b9229c4ea8e86cda02
Frederico De Paoli <fredericodepaoli@Fredericos-MBP.fios-router.home>
2018-10-08 00:46:44 -0400

    changed <a> to <Link>

--- src/components/project.js
+++ src/components/project.js
@@ -1,18 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../css/project.css'
 
 class Project extends React.Component {
   render() {
     const { title, summary, languages } = this.props
     return (
-      <a className="box-link" href={`/projects/${title}`}>
+      <Link className="box-link" to={`/projects/${title}`}>
         <div className="project-box">
-        <h3 className="title">{title}</h3>
-        <br></br>
-        <p className="summary">{summary}</p>
-        <p className="languages">Languages Used: {languages.join(', ')}</p>
+          <h3 className="title">{title}</h3>
+          <br></br>
+          <p className="summary">{summary}</p>
+          <p className="languages">Languages Used: {languages.join(', ')}</p>
         </div>
-      </a>
+      </Link>
     )
   }
 }
